4fa333275455695700e3878cf14f67762455dae1
Aman <amangarg0303@gmail.com>
2023-09-21 00:15:08 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,10 +6,35 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const modelViewer = {
+    backgroundColor: "#eee",
+    overflowX: "hidden",
+    posterColor: "#eee",
+    width: 300,
+    height: 300,
+  };
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <div className="flex justify-center">
+          <model-viewer
+            className="modelviewer"
+            style={modelViewer}
+            src={
+              "https://res.cloudinary.com/dzh0wkv97/image/upload/v1695231913/AR%20glbs/SLZ_2_ubbyf5.glb"
+            }
+            alt="Model"
+            ar
+            auto-rotate
+            camera-controls
+            touch-action="pan-y"
+            shadow-intensity="1"
+            poster={
+              "https://res.cloudinary.com/dzh0wkv97/image/upload/v1695231965/AR%20glbs/Mercedes_pbnqau.png"
+            }
+          ></model-viewer>
+        </div>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/product/:id" element={<SingleProductPage />} />
